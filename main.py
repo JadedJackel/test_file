b27612a3045627f2637123b9f88bdbba0bec3340
JadedJackel <JadedJackel@outlook.com>
2025-07-21 05:50:02 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,70 @@
+"""
+Project: GitHub - Practice
+Date: 07-01-25
+Purpose: Ehh, nothing really
+"""
+
+
 def main():
     print("Hello, World!")
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
